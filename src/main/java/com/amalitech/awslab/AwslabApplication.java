05a9289604bece7cfe18029597974c104d265cae
thenoblet <appiahpatricknoblet@gmail.com>
2025-09-05 14:39:51 +0000

    add initial project structure

---
+++ src/main/java/com/amalitech/awslab/AwslabApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.awslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwslabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalitech/awslab/AwslabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.awslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwslabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
